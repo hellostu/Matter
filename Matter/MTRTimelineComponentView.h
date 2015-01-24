ff5de7424996c698dc51c8633b41975fec792418
Stuart Lynch <stu@iname.com>
2015-01-24 09:41:04 +0000

    Added timeline component view

---
+++ Matter/MTRTimelineComponentView.h
@@ -0,0 +1,13 @@
+//
+//  MTRTimelineComponentView.h
+//  Matter
+//
+//  Created by Stuart Lynch on 24/01/2015.
+//  Copyright (c) 2015 DBHackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTRTimelineComponentView : UIView
+
+@end
